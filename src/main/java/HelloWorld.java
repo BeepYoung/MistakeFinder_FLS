51540de50695245f2e43f61bb84bb6d4c5d4d166
Vladimir Ermoshin <beepultimate@gmail.com>
2018-05-08 17:54:55 +0300

    To Check

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,47 +1,35 @@
-
-
-
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.DataFrame;
-import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.SQLContext;
-import scala.Tuple2;
+import org.apache.spark.sql.types.StructType;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
+
+//hdfs://quickstart.cloudera:8020/user/root/... path
+
 
 public class HelloWorld {
     public  static void main(String args[]){
-        SparkConf conf = new SparkConf().setAppName("someApp").setMaster("local");
+        SparkConf conf = new SparkConf().setAppName("someApp").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        Integer NUM_SAMPLES = 100;
-        List<Integer> l = new ArrayList<>(NUM_SAMPLES);
-        for (int i = 0; i < NUM_SAMPLES; i++) {
-            l.add(i);
-        }
+        SQLContext sqlContext = new org.apache.spark.sql.SQLContext(sc);
 
-        long count = sc.parallelize(l).filter(i -> {
-            double x = Math.random();
-            double y = Math.random();
-            return x*x + y*y < 1;
-        }).count();
-        System.out.println("Pi is roughly " + 4.0 * count / NUM_SAMPLES);
-        int inChar;
-        System.out.println("Enter a Character:");
-        try {
-            inChar = System.in.read();
-            System.out.print("You entered ");
-            System.out.println(inChar);
-        }
-        catch (IOException e){
-            System.out.println("Error reading from user");
-        }
-        System.out.println("Hello");
+        DataFrame df = sqlContext.read()
+                .format("com.databricks.spark.csv")
+                .option("inferSchema","true")
+                .option("header","true")
+                .load(args[0]);
+
+        /*DataFrame df = sqlContext.read()
+                .json(args[0]);*/
+        df.show();
+        df.printSchema();
+        /*JavaRDD<String> lines = sc.textFile(args[0]);
+        JavaRDD<Integer> lineLengths = lines.map(s -> s.length());
+        int totalLength = lineLengths.reduce((a, b) -> a + b);
+        System.out.println("\n\n\nTotal length is "+totalLength+"\n\n\n");*/
     }
 }
